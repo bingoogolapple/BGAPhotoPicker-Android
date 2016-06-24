6716d4db168273ab64fe9528c6c3912913792b6f
bingoogolapple <bingoogolapple@gmail.com>
2016-06-24 12:06:02 +0800

    Initial commit

---
+++ demo/src/main/java/cn/bingoogolapple/photopicker/demo/MainActivity.java
@@ -0,0 +1,18 @@
+package cn.bingoogolapple.photopicker.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+
+    }
+}
